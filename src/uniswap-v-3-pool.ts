6826e25c53ab2c63f81d72747535d40cb4c39382
loc <l0c4t0r@gmail.com>
2025-01-09 08:43:53 +0000

    initial

--- src/uniswap-v-3-pool.ts
+++ src/uniswap-v-3-pool.ts
@@ -1,162 +1,12 @@
+import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Burn as BurnEvent,
-  Collect as CollectEvent,
-  CollectProtocol as CollectProtocolEvent,
-  Flash as FlashEvent,
-  IncreaseObservationCardinalityNext as IncreaseObservationCardinalityNextEvent,
-  Initialize as InitializeEvent,
-  Mint as MintEvent,
-  SetFeeProtocol as SetFeeProtocolEvent,
-  Swap as SwapEvent
+  Swap as SwapEvent,
+  UniswapV3Pool
 } from "../generated/UniswapV3Pool/UniswapV3Pool"
 import {
-  Burn,
-  Collect,
-  CollectProtocol,
-  Flash,
-  IncreaseObservationCardinalityNext,
-  Initialize,
-  Mint,
-  SetFeeProtocol,
   Swap
 } from "../generated/schema"
 
-export function handleBurn(event: BurnEvent): void {
-  let entity = new Burn(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.tickLower = event.params.tickLower
-  entity.tickUpper = event.params.tickUpper
-  entity.amount = event.params.amount
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleCollect(event: CollectEvent): void {
-  let entity = new Collect(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.recipient = event.params.recipient
-  entity.tickLower = event.params.tickLower
-  entity.tickUpper = event.params.tickUpper
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleCollectProtocol(event: CollectProtocolEvent): void {
-  let entity = new CollectProtocol(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.recipient = event.params.recipient
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleFlash(event: FlashEvent): void {
-  let entity = new Flash(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.recipient = event.params.recipient
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
-  entity.paid0 = event.params.paid0
-  entity.paid1 = event.params.paid1
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleIncreaseObservationCardinalityNext(
-  event: IncreaseObservationCardinalityNextEvent
-): void {
-  let entity = new IncreaseObservationCardinalityNext(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.observationCardinalityNextOld =
-    event.params.observationCardinalityNextOld
-  entity.observationCardinalityNextNew =
-    event.params.observationCardinalityNextNew
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleInitialize(event: InitializeEvent): void {
-  let entity = new Initialize(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sqrtPriceX96 = event.params.sqrtPriceX96
-  entity.tick = event.params.tick
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleMint(event: MintEvent): void {
-  let entity = new Mint(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.owner = event.params.owner
-  entity.tickLower = event.params.tickLower
-  entity.tickUpper = event.params.tickUpper
-  entity.amount = event.params.amount
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSetFeeProtocol(event: SetFeeProtocolEvent): void {
-  let entity = new SetFeeProtocol(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.feeProtocol0Old = event.params.feeProtocol0Old
-  entity.feeProtocol1Old = event.params.feeProtocol1Old
-  entity.feeProtocol0New = event.params.feeProtocol0New
-  entity.feeProtocol1New = event.params.feeProtocol1New
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
 
 export function handleSwap(event: SwapEvent): void {
   let entity = new Swap(
@@ -169,10 +19,31 @@ export function handleSwap(event: SwapEvent): void {
   entity.sqrtPriceX96 = event.params.sqrtPriceX96
   entity.liquidity = event.params.liquidity
   entity.tick = event.params.tick
-
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
+  entity.reverted = false
+
+  const contract = UniswapV3Pool.bind(event.address)
+
+  entity.feeGrowthGlobal0X128 = contract.feeGrowthGlobal0X128()
+  entity.feeGrowthGlobal1X128 = contract.feeGrowthGlobal1X128()
+
+  // const try_0 = contract.try_feeGrowthGlobal0X128()
+  // if (try_0.reverted) {
+  //   entity.reverted = true
+  //   entity.feeGrowthGlobal0X128 = BigInt.fromI32(0)
+  // } else {
+  //   entity.feeGrowthGlobal0X128 = try_0.value
+  // }
+
+  // const try_1 = contract.try_feeGrowthGlobal1X128()
+  // if (try_1.reverted) {
+  //   entity.reverted = true
+  //   entity.feeGrowthGlobal1X128 = BigInt.fromI32(0)
+  // } else {
+  //   entity.feeGrowthGlobal1X128 = try_1.value
+  // }
 
   entity.save()
 }
